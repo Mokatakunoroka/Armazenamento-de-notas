91f4d72d2c439b0d62a1769727fd1aa65c683cd0
Luis Fernando <deliciasfrancesas16@gmail.com>
2026-03-06 19:03:07 -0300

    Pequena correção no Main.

--- Program.cs
+++ Program.cs
@@ -9,16 +9,12 @@ class Program
         Console.OutputEncoding = System.Text.Encoding.UTF8; //Aceitar a saída de dados em UTF-8
         Console.InputEncoding = System.Text.Encoding.UTF8; //Aceitar a entrada de dados em UTF-8
         Console.Clear();
-        /*var Manager = new Manager(new Helper());
+        var Manager = new Manager(new Helper());
         Manager.IniciarMenu = AllEnums.EscolherMenu.MenuInicial;
         bool ativo = true;
         while (ativo)
         {
             Manager.ExecutarMenu();
-        }*/
-        var Nota = new Nota(
-                   new StringHelper(new Manager(new Helper()), new Helper()),
-                   new Manager(new  Helper()), new Helper());
-        Console.WriteLine(Nota.TransStringFloat("a"));
+        }
     }
 }
